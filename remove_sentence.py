20b388f354005d607e461ba99036aab19fe526eb
Taryar <68702594+taryarlinnthit@users.noreply.github.com>
2023-04-06 21:25:24 +0630

    Add files via upload

---
+++ remove_sentence.py
@@ -0,0 +1,5 @@
+sentence = input("Enter sentence:")
+print("The sentence you entered is:")
+print(sentence)
+num = int(input("Enter the number of characters you'd like to remove:"))
+print(sentence[num:])
